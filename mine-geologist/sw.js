c53ef04ad882f7b91286705a0e7de351f4f17306
yay460-jpg <yay460@gmail.com>
2026-09-02 09:49:56 +0700

    Update sw.js

--- mine-geologist/sw.js
+++ mine-geologist/sw.js
@@ -11,7 +11,7 @@
  * CACHE_NAME harus diubah setiap deploy versi baru agar browser mendownload ulang.
  * ============================================================ */
 
-const CACHE_NAME = 'mine-geologist-build-20260902c';
+const CACHE_NAME = 'mine-geologist-build-20260902b';
 
 const APP_SHELL = [
   './',
